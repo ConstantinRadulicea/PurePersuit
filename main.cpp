033f26fa94de3d43f36b08f8ff674117e836831b
Badboy <constantinradulicea@gmail.com>
2024-01-23 12:32:05 +0200

    updates

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include "PurePursuitGeometry.h"
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
+#define MIN_SPEED (int)100
+#define MAX_SPEED (int)150
+#define SCREEN_CENTER_X (int)(78.0f / 2.0f)
+#define IMAGE_MAX_X 78
+#define IMAGE_MAX_Y 51
+#define LANE_WIDTH_PIXELS 50
 
+#define STEERING_SERVO_ANGLE_MIDDLE     85    // 90 middle
+#define STEERING_SERVO_ANGLE_MAX_RIGHT  0    // 38 max right
+#define STEERING_SERVO_ANGLE_MAX_LEFT   180   // 135 max left
+#define STEERING_SERVO_MAX_ANGLE MAX(fabsf(STEERING_SERVO_ANGLE_MIDDLE - STEERING_SERVO_ANGLE_MAX_RIGHT), fabsf(STEERING_SERVO_ANGLE_MIDDLE - STEERING_SERVO_ANGLE_MAX_LEFT))
 
 int main() {
 	points2lineABC(Point2D{ 48, 36 }, Point2D{ 12, 10 });
-	LineABC line1, line2;
+	LineABC line1, line2, line3, line4;
 
 	LineMQ wayPoints = { -3.5f, 625 };
 	// LineABC wayPointsAbc = { 3.5f, 1, -625 };
@@ -17,9 +29,14 @@ int main() {
 
 	float steeringAngle;
 
+	line1 = points2lineABC(Point2D{ 36, 45 }, Point2D{ 53, 48 });
+	line2 = parallelLineAtDistance(line1, 50, 1);
+	bisectorsOfTwoLines(line1, line2, &line3, NULL);
+
+
 
 	info = purePursuitCompute(carPos, wayPoints, carLength, lookAheadDistance);
-	info2 = purePursuitComputeABC(carPos, wayPointsAbc, carLength, lookAheadDistance);
+	info2 = purePursuitComputeABC(carPos, line3, carLength, lookAheadDistance);
 
 	carLength = angleBetweenLinesABC(points2lineABC(Point2D{ -1, -1 }, Point2D{-2, -2}), yAxisABC());
 	
